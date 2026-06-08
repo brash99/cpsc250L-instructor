486483fc9dc93d20fc3a6b4d2ced4cefd1ad3706
Edward Brash <brash@jlab.org>
2026-06-08 11:55:33 -0400

    Update lab 5

--- labs/lab05_classes_feature_branches/starter_code/main.py
+++ labs/lab05_classes_feature_branches/starter_code/main.py
@@ -31,10 +31,10 @@ def main():
 
     for student in [alice, ben, carlos]:
         print(f"{student.name}: "
-              f"Average: {student.calculate_average():.2f} "
-              f"High: {student.highest_score():.2f} "
-              f"Low: {student.lowest_score():.2f} "
-              f"Grade: {student.letter_grade()}")
+              f"    Average: {student.calculate_average():.2f} "
+              f"    High: {student.highest_score():.2f} "
+              f"    Low: {student.lowest_score():.2f} "
+              f"    Grade: {student.letter_grade()}")
 
 
 main()
